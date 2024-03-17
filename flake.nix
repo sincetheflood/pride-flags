912a5576a6bd7fc11b9473d568653bf40febade4
Ophelia Rozniak <dev@ophelia.ink>
2024-03-17 16:30:34 -0500

    Add nix flake files to avoid a direnv error
    Ran into issues where direnv would fail and print the following error:
        error: getting status of <NIXPATH>: No such file or directory
    
    This issue can (and seems to be, so far) be resolved by committing the
    flake files to git per this post on the NixOS forums:
    https://discourse.nixos.org/t/nix-flakes-nix-store-source-no-such-file-or-directory/17836/11

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+          pkgs.librsvg
+          pkgs.oxipng
+        ];
+      };
+    };
+}
